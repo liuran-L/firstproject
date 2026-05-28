9e5b88f9ca3588df68f2154f85c6992ddbeb0e27
LR <1433563175@qq.com>
2026-05-28 17:54:19 +0800

    first commit

---
+++ src/change.java
@@ -0,0 +1,9 @@
+public class change {
+    public static void main(String[] args) {
+        int age = 18;
+        double price=3.14;
+        String name ="张三";
+        char aaa = 'a';
+        boolean aab = true;
+    }
+}
