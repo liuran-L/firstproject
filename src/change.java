95fb210d66a5a36f8cd12d59fa8ee8f6de1b48b7
liuran-L <1433563175@qq.com>
2026-05-29 18:26:28 +0800

    Update change.java
    修改了人名

--- src/change.java
+++ src/change.java
@@ -2,7 +2,7 @@ public class change {
     public static void main(String[] args) {
         int age = 18;
         double price=3.14;
-        String name ="张三";
+        String name ="李四";
         char aaa = 'a';
         boolean aab = true;
     }
